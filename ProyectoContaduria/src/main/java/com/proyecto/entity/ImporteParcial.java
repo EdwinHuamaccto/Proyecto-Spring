a8aefc5e749e14d3788e58755848cb4974f23684
Edwin <a>
2022-10-31 10:49:42 -0500

    Agregando clases

---
+++ ProyectoContaduria/src/main/java/com/proyecto/entity/ImporteParcial.java
@@ -0,0 +1,29 @@
+package com.proyecto.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_importe_parcial")
+public class ImporteParcial {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="cod_imp_par")
+	private Integer codigoImporte;
+	@Column(name="importe_parcial")
+	private String importe;
+	@Column(name="monto")
+	private double monto;
+
+	/*	@ManyToOne
+	@JoinColumn(name = "cod_tipo")
+	private Tipo tipo;
+	@ManyToOne
+	@JoinColumn(name = "cod_detalle")
+	private Detalle detalle;
+	*/
+}
---
+++ ProyectoContaduria/src/main/java/com/proyecto/entity/LibroDiario.java
@@ -0,0 +1,58 @@
+package com.proyecto.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_libro_diario")
+public class LibroDiario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="cod_LDiario")
+	private int codigo;
+	@Column(name="fecha")
+	private Date fecha;
+			  
+	@ManyToOne
+	@JoinColumn(name = "cod_imp_par")
+	private ImporteParcial importe;
+	@ManyToOne
+	@JoinColumn(name = "cod_emp")
+	private Empleado empleado;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public ImporteParcial getImporte() {
+		return importe;
+	}
+	public void setImporte(ImporteParcial importe) {
+		this.importe = importe;
+	}
+	public Empleado getEmpleado() {
+		return empleado;
+	}
+	public void setEmpleado(Empleado empleado) {
+		this.empleado = empleado;
+	}
+			 
+		
+}
