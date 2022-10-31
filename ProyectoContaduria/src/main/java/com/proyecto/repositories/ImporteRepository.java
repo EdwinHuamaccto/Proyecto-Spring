c0b5148a381f51e478e0eab546e10f0bce1e0ea2
Edwin <a>
2022-10-31 18:19:07 -0500

    cambiando el interfaz

--- ProyectoContaduria/src/main/java/com/proyecto/repositories/ImporteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyecto.entity.ImporteParcial;
-
-public interface ImporteRepository extends JpaRepository<ImporteParcial, Integer>{
-
-}
