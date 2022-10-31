40f30f9fdcb9105a23a385755f82a6df5fd2152c
Edwin <a>
2022-10-31 11:11:50 -0500

    Agregando paquetes

--- ProyectoContaduria/src/main/java/com/proyecto/entity/ImporteParcial.java
+++ ProyectoContaduria/src/main/java/com/proyecto/entity/ImporteParcial.java
@@ -1,11 +1,17 @@
 package com.proyecto.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 
 @Entity
 @Table(name="tb_importe_parcial")
@@ -19,11 +25,14 @@ public class ImporteParcial {
 	@Column(name="monto")
 	private double monto;
 
-	/*	@ManyToOne
+    @ManyToOne
 	@JoinColumn(name = "cod_tipo")
 	private Tipo tipo;
+   
 	@ManyToOne
 	@JoinColumn(name = "cod_detalle")
 	private Detalle detalle;
-	*/
+	
+	@OneToMany(mappedBy = "importe")
+	private List<LibroDiario> listaLibroD;
 }
