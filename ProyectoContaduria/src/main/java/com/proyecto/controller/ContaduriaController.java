6f7908d13552b11e09128d44af674856753d22e1
Edwin <a>
2022-10-31 16:51:13 -0500

    cambios

--- ProyectoContaduria/src/main/java/com/proyecto/controller/ContaduriaController.java
+++ ProyectoContaduria/src/main/java/com/proyecto/controller/ContaduriaController.java
@@ -1,8 +1,22 @@
 package com.proyecto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/contador")
-public class ContaduriaController {
+import com.proyecto.service.TipoService;
 
+@Controller
+@RequestMapping("/LibroDiario")
+public class ContaduriaController {
+@Autowired
+private TipoService serviceTipo;
+@RequestMapping("/lista")
+public String inicio(Model model) {
+	model.addAttribute("tipos",serviceTipo.listar());
+
+	return "LibroDiario";
+}
+	
 }
