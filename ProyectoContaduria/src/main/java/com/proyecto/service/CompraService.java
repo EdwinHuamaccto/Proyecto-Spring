20dd4cddb1bfe141e77ae5cca5d6afc1958507d1
Edwin <a>
2022-10-31 19:48:36 -0500

    Agregando el menu

--- ProyectoContaduria/src/main/java/com/proyecto/service/CompraService.java
+++ ProyectoContaduria/src/main/java/com/proyecto/service/CompraService.java
@@ -3,10 +3,11 @@ package com.proyecto.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.proyecto.entity.Compra;
 import com.proyecto.repositories.CompraRepository;
-
+@Service
 public class CompraService {
 	@Autowired
 	private CompraRepository repo;
